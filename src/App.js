5611fd5515b180773af03ec742d8c6b5ca88e8e9
Agustin Boasso <boassoagustin@gmail.com>
2023-08-30 19:51:30 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Scene from "./components/Scene"
 import SceneDos from "./components/SceneDos";
 import SceneAmong from "./components/SceneAmong";
 import SolarSystem from "./components/SolarSystem";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Scene/>
-      {/* <SolarSystem/> */}
+      {/* <Scene/> */}
+      
+      
        {/* <SceneDos/>  */}
-      {/* <SceneAmong/> */}
+      <SceneAmong/>
     </div>
   );
 }
